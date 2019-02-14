b691f32e6ca81ec7a46ae9a2c584406a4d8cb271
EasterMaster <teo.mastropasqua@gmail.com>
2019-02-14 13:30:15 +0100

    tutto

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        // Phone
+        dir.y = Input.acceleration.x;
+
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+
+
+        // Tastiera
+        if (Input.GetKey(KeyCode.RightArrow) && transform.localEulerAngles.y > -50f)
+        {
+            dir = Vector3.up;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.localEulerAngles.y < 50f)
+        {
+            dir = Vector3.down;
+        }
+
+        dir *= Time.deltaTime * speed;
+
+        transform.Rotate(dir);
+    }
+
+    
+}
---
+++ Assets/Scripts/ThrowInput.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowInput : MonoBehaviour {
+
+    [SerializeField]
+    float minMagnitudeSlice;
+    Vector2 sliceVector;
+
+    Ray mouseRay;
+    RaycastHit mousePosition;
+
+    Vector2 startPosition;
+    Vector2 endPosition;
+    private void Update()
+    {
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(mouseRay, out mousePosition);
+
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPosition = touch.position;
+                Debug.Log("tacciato");
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                endPosition = touch.position;
+                sliceVector = endPosition - startPosition;
+                Debug.Log("lasciato");
+
+                if (sliceVector.magnitude > minMagnitudeSlice)
+                {
+                    throwObject();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            startPosition = mousePosition.point;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            endPosition = mousePosition.point;
+            sliceVector = endPosition - startPosition;
+            Debug.Log("lasciato " + sliceVector.magnitude);
+            if (sliceVector.magnitude > minMagnitudeSlice)
+            {
+                throwObject();
+            }
+        }
+    }
+
+    void throwObject()
+    {
+        Debug.Log("Lanciato");
+    }
+}
